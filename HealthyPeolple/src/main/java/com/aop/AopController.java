224c6560d66dd7149181958dc6232f18f62a1ffe
yumanshjuo <hreeneour@outlook.com>
2020-12-25 11:36:30 +0800

    healthy1.0(AOP)

--- HealthyPeolple/src/main/java/com/aop/AopController.java
+++ HealthyPeolple/src/main/java/com/aop/AopController.java
@@ -8,35 +8,11 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class AopController {
-    //这个方法定义了切入点
-    @Pointcut("@annotation(com.service.*)")
-    public void pointCut()  {}
-
-    //这个方法定义了具体的通知
-    @After("pointCut()")
-    public void recordRequestParam(JoinPoint joinPoint) {
+    @Before("execution(* com.service.UserService.*(..))")
+    public void BeforeT(JoinPoint joinPoint) {
         for (Object s : joinPoint.getArgs()) {
             //打印所有参数，实际中就是记录日志了
-            System.out.println("after advice : " + s);
+            System.out.println("Before(executionafter advice : " + s);
         }
-    }
-
-    //这个方法定义了具体的通知
-    @Before("pointCut()")
-    public void startRecord(JoinPoint joinPoint) {
-        for (Object s : joinPoint.getArgs()) {
-            //打印所有参数
-            System.out.println("before advice : " + s);
-        }
-    }
-
-    //这个方法定义了具体的通知
-    @Around("pointCut()")
-    public Object aroundRecord(ProceedingJoinPoint pjp) throws Throwable {
-        for (Object s : pjp.getArgs()) {
-            //打印所有参数
-            System.out.println("around advice : " + s);
-        }
-        return pjp.proceed();
     }
 }
--- HealthyPeolple/src/main/java/com/test.java
+++ HealthyPeolple/src/main/java/com/test.java
@@ -2,15 +2,16 @@ package com;
 
 import com.dao.UserDao;
 import com.pojo.User;
+import com.service.Impl.UserImpl;
 import com.service.UserService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class test {
-    @Autowired
-    UserService userDao;
+
     @Test
     public void test(){
+        UserService userDao = new UserImpl();
         userDao.test(1);
         System.out.println("end");
     }
